cccb49e7382587469899e4191a81e67eb10b8c29
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-10-30 15:37:10 +0900

    실습 02

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
-int main(int argc, char *argv[])
+int main(void)
 {
+  
+  int i, average;
+  int grade[SIZE];
+  int sum=0;
+  
+  printf("5명의 점수를 입력하세요: \n");
+  
+  for(i=0; i<SIZE; i++)
+  { 
+    scanf("%d", &grade[i]);
+    sum += grade[i];
+  }
+
+  //for(i=0; i<SIZE; i++)
+  //printf("grade[%d]=%d\n", i, grade[i]);
+  
+  average = sum/SIZE;
+  printf("성적 평균: %d\n", average);
   
   system("PAUSE");	
   return 0;
